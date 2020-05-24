5a4e3caa5a8f5d5033fc334a7d7d81157c1552ab
Herman <hfbmartinus@gmail.com>
2020-05-24 19:41:31 +0200

    Checking debug from env variable

--- textblog/settings.py
+++ textblog/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = (os.environ.get('DEBUG') == 'True')
 
 if DEBUG:
     ALLOWED_HOSTS = ['*']
