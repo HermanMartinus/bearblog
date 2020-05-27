175e564d87fbe07b71b78b9567d0043c7819ff45
gavinw <gavinwiener@gmail.com>
2020-05-27 14:13:54 +0800

    Re-order urls

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accounts/delete/', views.delete_user, name='user_delete'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/posts/', views.posts_edit, name='post'),
     path('dashboard/posts/new/', views.post_new, name='post_new'),
     path('dashboard/posts/<pk>/', views.post_edit, name='post_edit'),
     path('dashboard/posts/<pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
     path('blog/', views.posts, name='posts'),
-    path('accounts/delete/', views.delete_user, name='user_delete'),
     path('<slug>/', views.post, name='post'),
 ]
