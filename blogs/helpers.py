fd6e4863e399077cf883f13432aaf7344f3fb1ea
Herman <hfbmartinus@gmail.com>
2020-05-30 10:20:32 +0200

    Separated dashboard views from blog views

--- blogs/helpers.py
+++ blogs/helpers.py
@@ -79,6 +79,14 @@ def delete_domain(domain):
     print(response.text)
 
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 def unmark_element(element, stream=None):
     if stream is None:
         stream = StringIO()
--- blogs/urls.py
+++ blogs/urls.py
@@ -2,16 +2,19 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from . import views
+from . import dashboard_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('accounts/delete/', views.delete_user, name='user_delete'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('dashboard/domain/', views.domain_edit, name='domain'),
-    path('dashboard/posts/', views.posts_edit, name='post'),
-    path('dashboard/posts/new/', views.post_new, name='post_new'),
-    path('dashboard/posts/<pk>/', views.post_edit, name='post_edit'),
-    path('dashboard/posts/<pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
+    path('accounts/delete/', dashboard_views.delete_user, name='user_delete'),
+    path('dashboard/', dashboard_views.dashboard, name='dashboard'),
+    path('dashboard/domain/', dashboard_views.domain_edit, name='domain'),
+    path('dashboard/posts/', dashboard_views.posts_edit, name='post'),
+    path('dashboard/posts/new/', dashboard_views.post_new, name='post_new'),
+    path('dashboard/posts/<pk>/', dashboard_views.post_edit, name='post_edit'),
+    path('dashboard/posts/<pk>/delete/', dashboard_views.PostDelete.as_view(), name='post_delete'),
+    path('ip_test/', dashboard_views.ip_test, name='ip_test'),
+
     path('blog/', views.posts, name='posts'),
     path('<slug>/', views.post, name='post'),
 ]
