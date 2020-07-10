c6caa0dd0c433fce71d7b0b882981b56272484c4
Herman <hfbmartinus@gmail.com>
2020-07-10 18:02:31 +0200

    Prevent link from doing stupid things when linkified

--- blogs/templatetags/markdownify.py
+++ blogs/templatetags/markdownify.py
@@ -10,22 +10,26 @@ register = template.Library()
 
 @register.filter
 def markdown(value):
-    markdown = value
-
-    # linkify hashtags
-    for tag in re.findall(r"(#[\d\w\.]+)", markdown):
-        text_tag = tag.replace('#', '')
-        markdown = markdown.replace(
-            tag,
-            f"[{tag}](/blog/?q=%23{text_tag})")
+    markdown = linkify_hashtags(value)
 
     markup = mistune.html(markdown)
 
     soup = html_parser(markup, 'html.parser')
-    html_tags = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+    heading_tags = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
 
-    for each_tag in html_tags:
-        each_tag.attrs['id'] = f"section-{html_tags.index(each_tag)}"
+    for each_tag in heading_tags:
+        each_tag.attrs['id'] = f"section-{heading_tags.index(each_tag)}"
 
     cleaned_markup = clean_html(str(soup))
+
     return cleaned_markup
+
+
+def linkify_hashtags(markdown):
+    for tag in re.findall(r"(#[\d\w\.]+)", markdown):
+        if not re.search(r"\((#[\d\w\.]+)\)", tag):
+            text_tag = tag.replace('#', '')
+            markdown = markdown.replace(
+                tag,
+                f"[{tag}](/blog/?q=%23{text_tag})")
+    return markdown
