3ab6cd0a72856088db502515bdad133b0ae4de82
Herman <hfbmartinus@gmail.com>
2020-07-10 17:14:11 +0200

    Added section IDs to html

--- blogs/templatetags/markdownify.py
+++ blogs/templatetags/markdownify.py
@@ -1,7 +1,9 @@
 from django import template
 import mistune
+from bs4 import BeautifulSoup as html_parser
 from lxml.html.clean import clean_html
 import re
+
 
 register = template.Library()
 
@@ -18,6 +20,12 @@ def markdown(value):
             f"[{tag}](/blog/?q=%23{text_tag})")
 
     markup = mistune.html(markdown)
-    cleaned_markup = clean_html(markup)
 
+    soup = html_parser(markup, 'html.parser')
+    html_tags = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+
+    for each_tag in html_tags:
+        each_tag.attrs['id'] = f"section-{html_tags.index(each_tag)}"
+
+    cleaned_markup = clean_html(str(soup))
     return cleaned_markup
