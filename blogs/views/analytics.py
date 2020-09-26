6a68e6eddbd4b383c7001c978afc953517f7190d
Herman <hfbmartinus@gmail.com>
2020-09-26 12:25:46 +0200

    Added cool filtering for analytics

--- blogs/views/analytics.py
+++ blogs/views/analytics.py
@@ -1,9 +1,12 @@
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from datetime import timedelta
 
 from blogs.models import Blog, Hit, Post
+from django.core.exceptions import ObjectDoesNotExist
 from blogs.helpers import root
-from django.db.models import Count, Sum
+from django.db.models import Count, Sum, Q
 from django.http import HttpResponse
 
 from ipaddr import client_ip
@@ -13,8 +16,19 @@ from ipaddr import client_ip
 def analytics(request):
     blog = get_object_or_404(Blog, user=request.user)
 
+    if request.GET.get('days', ''):
+        days = int(request.GET.get('days', ''))
+    else:
+        days = 99999
+
+    time_threshold = timezone.now() - timedelta(days=days)
+
     posts = Post.objects.annotate(
-            hit_count=Count('hit')).filter(blog=blog, publish=True).order_by('-hit_count', '-published_date')
+            hit_count=Count('hit', filter=Q(hit__created_date__gt=time_threshold))).filter(
+                blog=blog,
+                publish=True,
+                published_date__gt=time_threshold,
+                ).order_by('-hit_count', '-published_date')
 
     unique_reads = posts.aggregate(Sum('hit_count'))
 
