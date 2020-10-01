d06427ab67d405e8471fa7cece0e893a89317408
Herman <hfbmartinus@gmail.com>
2020-10-01 16:09:40 +0200

    Added custom date range filtering

--- blogs/views/analytics.py
+++ blogs/views/analytics.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from django.utils.dateparse import parse_date
 from datetime import timedelta
 
 from blogs.models import Blog, Hit, Post
@@ -15,29 +16,44 @@ from ipaddr import client_ip
 @login_required
 def analytics(request):
     blog = get_object_or_404(Blog, user=request.user)
+    time_threshold = False
+    date_from = False
+    date_to = False
+    
+    if request.GET.get('date_from', '') and request.GET.get('date_to', ''):
+        date_from = parse_date(request.GET.get('date_from', ''))
+        date_to = parse_date(request.GET.get('date_to', ''))
 
-    if request.GET.get('days', ''):
-        days = int(request.GET.get('days', ''))
+        posts = Post.objects.annotate(
+                hit_count=Count('hit', filter=Q(hit__created_date__range=[date_from, date_to]))).filter(
+                    blog=blog,
+                    publish=True,
+                    ).order_by('-hit_count', '-published_date')
     else:
-        days = 1
+        if request.GET.get('days', ''):
+            days = int(request.GET.get('days', ''))
+        else:
+            days = 1
 
-    time_threshold = timezone.now() - timedelta(days=days)
-    delta = timezone.now() - blog.created_date
+        time_threshold = timezone.now() - timedelta(days=days)
 
-    posts = Post.objects.annotate(
-            hit_count=Count('hit', filter=Q(hit__created_date__gt=time_threshold))).filter(
-                blog=blog,
-                publish=True,
-                ).order_by('-hit_count', '-published_date')
+        posts = Post.objects.annotate(
+                hit_count=Count('hit', filter=Q(hit__created_date__gt=time_threshold))).filter(
+                    blog=blog,
+                    publish=True,
+                    ).order_by('-hit_count', '-published_date')
 
     unique_reads = posts.aggregate(Sum('hit_count'))
+    delta = timezone.now() - blog.created_date
 
     return render(request, 'dashboard/analytics.html', {
         'unique_reads': unique_reads,
         'posts': posts,
         'blog': blog,
         'time_threshold': time_threshold,
-        'since_started': delta.days
+        'since_started': delta.days,
+        'date_from': date_from,
+        'date_to': date_to
     })
 
 
