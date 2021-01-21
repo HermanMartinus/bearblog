70fedc7dd98f46798e90b3112ee192638ec28c0e
Herman Martinus <hfbmartinus@gmail.com>
2021-01-21 16:30:00 +0200

    Remove the favicon url

--- textblog/urls.py
+++ textblog/urls.py
@@ -15,10 +15,6 @@ urlpatterns = [
         "robots.txt",
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
     ),
-    path(
-        "favicon.ico",
-        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
-    ),
 ]
 
 if settings.DEBUG:
