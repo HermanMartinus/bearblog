383d0c911b98ba92edb186c820c734d6f532c112
Herman Martinus <hfbmartinus@gmail.com>
2021-01-21 16:15:58 +0200

    Added a favicon to download if necessary

--- textblog/urls.py
+++ textblog/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.conf import settings
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('mothership/', admin.site.urls),
@@ -12,6 +14,10 @@ urlpatterns = [
     path(
         "robots.txt",
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
     ),
 ]
 
