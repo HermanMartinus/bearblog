f8df4d287ea37a44341be875ae920a75fbe730b9
Herman Martinus <hfbmartinus@gmail.com>
2021-03-16 16:57:33 +0200

    Added gzip compression

--- textblog/settings.py
+++ textblog/settings.py
@@ -44,6 +44,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 MIDDLEWARE = [
+    'django.middleware.gzip.GZipMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
