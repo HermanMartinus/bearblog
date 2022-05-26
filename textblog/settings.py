4273d2d2304ceee82a1197779e667b60953d9132
Herman Martinus <hfbmartinus@gmail.com>
2022-05-26 15:21:06 +0800

    cleanup: removed unnecessary declaration

--- textblog/settings.py
+++ textblog/settings.py
@@ -134,7 +134,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
