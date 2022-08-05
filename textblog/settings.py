f5b2140f2d319a8e775a817ca9ad97806d25adf8
Herman Martinus <hfbmartinus@gmail.com>
2022-08-05 12:40:53 +0200

    update: add humanise

--- textblog/settings.py
+++ textblog/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'blogs.apps.BlogsConfig',
     'allauth.account',
     'allauth.socialaccount',
