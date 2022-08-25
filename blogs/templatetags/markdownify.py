809a7e69efdaf44b498d8bbedb44068401d43119
Herman Martinus <hfbmartinus@gmail.com>
2022-08-25 14:33:13 +0200

    update: added special markers and email subs inline

--- blogs/templatetags/markdownify.py
+++ blogs/templatetags/markdownify.py
@@ -19,14 +19,17 @@ def markdown(content):
     soup = html_parser(markup, 'html.parser')
     heading_tags = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
 
+    # Give headings IDs so they can be linked to 
     for each_tag in heading_tags:
         each_tag.attrs['id'] = slugify(each_tag.text)
 
+    # Create new-tab links
     for each_anchor in soup.find_all('a', href=True):
         if 'tab:' in each_anchor.attrs['href']:
             each_anchor.attrs['href'] = each_anchor.attrs['href'].replace('tab:', '')
             each_anchor.attrs['target'] = '_blank'
 
+    # Add pre to codeblocks and order correctly 
     for match in soup.findAll('code'):
         if match.parent.name == 'pre':
             if match.has_attr('class'):
@@ -37,6 +40,25 @@ def markdown(content):
                 new_tag = soup.new_tag("code")
                 new_tag.append(html.escape(str(match.contents[0])))
                 match.replace_with(new_tag)
+
+    # TODO this only works if it's the last child in the dom branch
+    for match in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a', 'span', 'b', 'i', 'strong', 'em']):
+        if match.string and '<code>' not in str(match):
+            # Typographic replacement of special characters
+            match.string.replace_with(match.string.replace(
+                '(c)', '©').replace(
+                    '(C)', '©').replace(
+                        '(r)', '®').replace(
+                            '(R)', '®').replace(
+                                '(tm)', '™').replace(
+                                    '(TM)', '™').replace(
+                                        '(p)', '℗').replace(
+                                            '(P)', '℗').replace(
+                                                '+-', '±'))
+
+            # Add email subscription form inline
+            match.string.replace_with(
+                match.string.replace('{{ email-signup }}', template.loader.render_to_string('snippets/email_subscribe_form.html')))
 
     safe_attrs = list(lxml.html.clean.defs.safe_attrs) + ['style', 'controls']
     lxml.html.clean.defs.safe_attrs = safe_attrs
@@ -51,8 +73,5 @@ def markdown(content):
         'stackblitz.com']
     cleaner = Cleaner(host_whitelist=host_whitelist, safe_attrs=safe_attrs)
     cleaned_markup = cleaner.clean_html(str(soup))
-
-    cleaned_markup = cleaned_markup.replace('{{ email-signup }}', template.loader.render_to_string('snippets/email_subscribe_form.html'))
-    # TODO: add 'sandbox' attribute to all iframes
 
     return cleaned_markup
