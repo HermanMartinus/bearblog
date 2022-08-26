790b90f927732ddc5ed5d3e8321a4281e3346d1c
Herman Martinus <hfbmartinus@gmail.com>
2022-08-26 09:58:01 +0200

    test: explicit trusted origin

--- textblog/settings.py
+++ textblog/settings.py
@@ -17,7 +17,7 @@ DEFAULT_LOGGING['handlers']['console']['filters'] = []
 ALLOWED_HOSTS = ['*']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_TRUSTED_ORIGINS = ["https://*", "http://*"]
+CSRF_TRUSTED_ORIGINS = ["https://blog.justcors.com"]
 # SECURE_SSL_REDIRECT = True
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
