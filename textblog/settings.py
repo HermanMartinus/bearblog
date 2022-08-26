4f3740436612cf779b5e28db001fa2397bc8f268
Herman Martinus <hfbmartinus@gmail.com>
2022-08-26 10:12:26 +0200

    update: cors origins allow all

--- textblog/settings.py
+++ textblog/settings.py
@@ -17,7 +17,8 @@ DEFAULT_LOGGING['handlers']['console']['filters'] = []
 ALLOWED_HOSTS = ['*']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1', 'https://159.223.204.176']
+CORS_ORIGIN_ALLOW_ALL = True
+# CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1', 'https://159.223.204.176']
 # SECURE_SSL_REDIRECT = True
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
