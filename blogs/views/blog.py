aeb1257d79e1ff6b534e9e069ba1f6933dc703e3
Herman Martinus <hfbmartinus@gmail.com>
2022-09-07 12:50:04 +0200

    test: use utf8 encoding

--- blogs/views/blog.py
+++ blogs/views/blog.py
@@ -216,8 +216,8 @@ def public_analytics(request):
 
 @csrf_exempt
 def lemon_webhook(request):
-    digest = hmac.new(settings.LEMONSQUEEZY_SIGNATURE.encode(), msg=request.body, digestmod=hashlib.sha256).digest()
-    signature = base64.b64encode(digest).decode()
+    digest = hmac.new(settings.LEMONSQUEEZY_SIGNATURE.encode('utf-8'), msg=request.body, digestmod=hashlib.sha256).digest('utf-8')
+    signature = base64.b64encode(digest).decode('utf-8')
     print(signature)
     # digest = hmac.new(settings.LEMONSQUEEZY_SIGNATURE, msg=request.body, digestmod=hashlib.sha256).digest()
     # signature = base64.b64encode(digest).decode()
