8045471dc0c876615a9f4be5d40ea5eb917092c8
Herman Martinus <hfbmartinus@gmail.com>
2022-11-10 16:28:32 +0200

    test: see what causes H18

--- blogs/views/blog.py
+++ blogs/views/blog.py
@@ -137,6 +137,8 @@ def post(request, slug):
     # Workaround to prevent H18 errors on post requests
     if request.POST:
         body = str(request.body)
+        print(blog)
+        print(body)
 
     # Check for a custom blogreel path and render the blog page
     if slug == blog.blog_path:
