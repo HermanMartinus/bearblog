65ae97ac8022e2e32e849a4d283d26531182eb84
Herman Martinus <hfbmartinus@gmail.com>
2023-02-26 11:17:54 +0200

    update: added to the review list

--- blogs/views/staff.py
+++ blogs/views/staff.py
@@ -110,7 +110,7 @@ def review_flow(request):
             delay_period = timezone.now() - timedelta(days=1)
 
             # Filtering Discord about me pages
-            strings_to_check = ['ooc', 'infp', 'she/her', 'he/him', 'they/them', 'masc terms', 'fem terms', 'dni', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
+            strings_to_check = ['ooc', 'infp', 'she/her', 'he/him', 'they/them', 'masc terms', 'fem terms', 'pronouns', 'dni', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
             if any(string.lower() in blog.content.replace('.', ' ').lower() for string in strings_to_check):
                 blog.reviewed = True
                 blog.save()
