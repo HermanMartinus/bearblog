4d2f2f05ca5afa30cac397646be0561404db58ee
Herman Martinus <hfbmartinus@gmail.com>
2023-03-02 09:39:56 +0200

    update: more verbose response message

--- blogs/views/blog.py
+++ blogs/views/blog.py
@@ -273,7 +273,7 @@ def add_order_id(request):
         if blog.upgraded:
             blog.order_id = request.POST.get("order_id")
             blog.save()
-            return HttpResponse(f"Added order_id to {blog}")
+            return HttpResponse(f"Added order_id {blog.order_id} to {blog}")
         else:
             return HttpResponse(f"{blog} is not upgraded")
 
