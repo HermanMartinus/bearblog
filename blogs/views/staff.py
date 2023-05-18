c3a10069da69a8ebbae9fec1058892e8e85f3400
Herman Martinus <hfbmartinus@gmail.com>
2023-05-18 13:20:08 +0200

    update: that was wrong

--- blogs/views/staff.py
+++ blogs/views/staff.py
@@ -117,7 +117,7 @@ def get_empty_blogs():
     # Most recent 100
     timeperiod = timezone.now() - timedelta(weeks=2)
     empty_blogs = Blog.objects.annotate(num_posts=Count('post')).annotate(content_length=Length('content')).filter(
-        last_modified__lte=timeperiod, num_posts__lte=0, content_length__lt=20, upgraded=False).exclude(custom_styles=None).order_by('created_date')[:100]
+        last_modified__lte=timeperiod, num_posts__lte=0, content_length__lt=20, upgraded=False).order_by('created_date')[:100]
 
     return empty_blogs
 
