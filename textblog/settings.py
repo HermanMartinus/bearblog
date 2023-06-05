94cc80e927b812733c49a4528bf735e92e57726f
Herman Martinus <hfbmartinus@gmail.com>
2023-06-05 11:01:42 +0100

    update: move clacks to middleware

--- textblog/settings.py
+++ textblog/settings.py
@@ -96,6 +96,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'blogs.middleware.XClacksOverheadMiddleware'
 ]
 
 ROOT_URLCONF = 'textblog.urls'
