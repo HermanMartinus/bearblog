2780b58e9238f7ef0691c2cf97072c96b31aede7
Herman Martinus <hfbmartinus@gmail.com>
2023-11-08 12:13:23 +0200

    update: remove unused db fields

--- blogs/models.py
+++ blogs/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 
-from taggit.managers import TaggableManager
 from math import log
 
 
