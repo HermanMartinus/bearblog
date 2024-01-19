1f81221b1e01e9a56f3a6117d228e65c6a284dd1
Herman Martinus <hfbmartinus@gmail.com>
2024-01-19 07:59:21 +0200

    update: remove feed cleaning

--- blogs/views/feed.py
+++ blogs/views/feed.py
@@ -40,7 +40,7 @@ def feed(request):
         fe.title(post.title)
         fe.author({'name': name, 'email': 'hidden'})
         fe.link(href=f"{blog.useful_domain}/{post.slug}/")
-        fe.content(markdown(clean_string(post.content.replace('{{ email-signup }}', '')), blog), type="html")
+        fe.content(markdown(post.content.replace('{{ email-signup }}', ''), blog), type="html")
         fe.published(post.published_date)
         fe.updated(post.published_date)
 
