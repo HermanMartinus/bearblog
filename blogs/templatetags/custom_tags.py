77bb06730cf059dca1cb475de451bf495cf64f71
Herman Martinus <hfbmartinus@gmail.com>
2024-04-26 13:29:38 +0200

    update: embedded posts should respect future date

--- blogs/templatetags/custom_tags.py
+++ blogs/templatetags/custom_tags.py
@@ -214,7 +214,7 @@ def element_replacement(markup, blog, post=None):
             elif 'content:' in param[0] and not post or post.is_page:
                 content = param[5] == 'True'
 
-        filtered_posts = apply_filters(blog.posts.filter(publish=True, is_page=False), tag, limit, order)
+        filtered_posts = apply_filters(blog.posts.filter(publish=True, is_page=False, published_date__lte=timezone.now()), tag, limit, order)
         context = {'blog': blog, 'posts': filtered_posts, 'embed': True, 'show_description': description, 'show_content': content}
         return render_to_string('snippets/post_list.html', context)
 
