8b52911754df136614db92c518a0762461b938d1
Herman Martinus <hfbmartinus@gmail.com>
2024-06-22 11:16:41 +0700

    update: adding some prints for checking cache invalidation

--- blogs/models.py
+++ blogs/models.py
@@ -161,7 +161,8 @@ class Blog(models.Model):
         # Invalidate feed cache
         CACHE_KEY = f'{self.subdomain}_all_posts'
         cache.delete(CACHE_KEY)
-        
+        print(f"Cache invalidated for key: {self.useful_domain}")        
+
         super(Blog, self).save(*args, **kwargs)
 
     def __str__(self):
