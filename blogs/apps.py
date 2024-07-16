04028a8e597d8d1deb4e488375c4b297b4603088
Herman Martinus <hfbmartinus@gmail.com>
2024-07-16 08:03:28 +0700

    update: added deploy log

--- blogs/apps.py
+++ blogs/apps.py
@@ -1,8 +1,13 @@
 from django.apps import AppConfig
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class BlogsConfig(AppConfig):
     name = 'blogs'
 
     def ready(self):
-        import blogs.hooks
+        print("ʕ≧ᴥ≦ʔ Bear has been activated!")
+        logger.log(1, "ʕ≧ᴥ≦ʔ Bear has been activated!", exc_info=True)
+
