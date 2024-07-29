ffaf30dea3d10ae5cb536e320a1f821306bda481
Herman Martinus <hfbmartinus@gmail.com>
2024-07-29 14:48:48 +0800

    update: use gravity modifier

--- blogs/models.py
+++ blogs/models.py
@@ -210,7 +210,7 @@ class Post(models.Model):
         self.upvotes = self.upvote_set.count()
 
         if self.upvotes > 1:
-            log_of_upvotes = log(self.upvotes, 13)
+            log_of_upvotes = log(self.upvotes, 10)
 
             posted_at = self.first_published_at or self.published_date
 
@@ -219,7 +219,7 @@ class Post(models.Model):
                 if self.blog.deprioritise or self.deprioritise:
                     score = 0
                 else:
-                    score = (log_of_upvotes) + ((seconds - 1577811600) / (14 * 86400))
+                    score = (log_of_upvotes) + ((seconds - 1577811600) / (20 * 86400))
                 self.score = score
     
     def save(self, *args, **kwargs):
