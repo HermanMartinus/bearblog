7f9ebfa6ecd1957f90857cf8d85b1d5b00edfce8
Herman Martinus <hfbmartinus@gmail.com>
2024-09-03 09:05:38 +0200

    hotfix: draft saving

--- blogs/models.py
+++ blogs/models.py
@@ -241,8 +241,8 @@ class Post(models.Model):
         if self.publish and self.first_published_at is None:
             self.first_published_at = self.published_date or timezone.now()
         
-        if self.published_date and self.published_date < self.first_published_at:
-            self.first_published_at = self.published_date
+        # if self.published_date and self.published_date < self.first_published_at:
+        #     self.first_published_at = self.published_date
 
         # Update the score for the discover feed
         self.update_score()
