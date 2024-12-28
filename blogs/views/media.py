3a4871b111c88253cf0b2cc28195e5dc2c5848da
Herman Martinus <hfbmartinus@gmail.com>
2024-12-28 07:33:54 +0200

    update: moved it to doc type

--- blogs/views/media.py
+++ blogs/views/media.py
@@ -22,10 +22,10 @@ from blogs.models import Blog, Media
 bucket_name = 'bear-images'
 
 
-image_types = ['png', 'jpg', 'jpeg', 'tiff', 'bmp', 'gif', 'svg', 'webp', 'avif', 'ico', 'heic', 'css']
+image_types = ['png', 'jpg', 'jpeg', 'tiff', 'bmp', 'gif', 'svg', 'webp', 'avif', 'ico', 'heic']
 video_types = ['mp4', 'webm', 'mkv']
 audio_types = ['mp3', 'ogg', 'wav', 'opus', 'flac']
-document_types = ['pdf', 'doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx', 'txt', 'rtf', 'epub', 'ps', 'odt', 'ods', 'odp', 'odg', 'odf', 'mml', 'odb', 'uot', 'uos', 'uop']
+document_types = ['pdf', 'doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx', 'txt', 'rtf', 'epub', 'ps', 'odt', 'ods', 'odp', 'odg', 'odf', 'mml', 'odb', 'uot', 'uos', 'uop', 'css']
 font_types = ['woff', 'woff2', 'ttf', 'otf']
 
 file_types = image_types + video_types + audio_types + document_types + font_types
