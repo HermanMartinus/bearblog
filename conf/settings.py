592227678165f8f01eb1fa0ef3127880f17a80c4
Herman Martinus <hfbmartinus@gmail.com>
2025-01-21 21:21:21 +0200

    hotfix: nope

--- conf/settings.py
+++ conf/settings.py
@@ -137,7 +137,7 @@ DATABASES = {
 }
 
 if os.getenv('DATABASE_URL'):
-    db_from_env = dj_database_url.config()
+    db_from_env = dj_database_url.config(conn_max_age=600)
     DATABASES['default'].update(db_from_env)
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB
