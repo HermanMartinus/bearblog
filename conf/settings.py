a9fcec8d74ffdff86706a68657a67f66ace90564
Herman Martinus <hfbmartinus@gmail.com>
2025-01-22 07:12:18 +0200

    hotfix: turned off redis

--- conf/settings.py
+++ conf/settings.py
@@ -143,16 +143,16 @@ if os.getenv('DATABASE_URL'):
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-if os.getenv('REDISCLOUD_URL'):
-    CACHES = {
-        'default': {
-            'BACKEND': 'django_redis.cache.RedisCache',
-            'LOCATION': os.getenv('REDISCLOUD_URL'),
-            'OPTIONS': {
-                'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            }
-        }
-    }
+# if os.getenv('REDISCLOUD_URL'):
+#     CACHES = {
+#         'default': {
+#             'BACKEND': 'django_redis.cache.RedisCache',
+#             'LOCATION': os.getenv('REDISCLOUD_URL'),
+#             'OPTIONS': {
+#                 'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#             }
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
