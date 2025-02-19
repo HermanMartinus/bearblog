e4af2ed2f8d3fc09aa2ed293dc8c5b33762363e0
Herman Martinus <hfbmartinus@gmail.com>
2025-02-19 08:54:04 +0200

    update: redirect apple-touch-icon to logo

--- conf/urls.py
+++ conf/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.generic import RedirectView
 
@@ -10,6 +10,9 @@ urlpatterns = [
     path('', include('blogs.urls')),
     path("favicon.ico", RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
     path("logo.png", RedirectView.as_view(url='/static/logo.png', permanent=True)),
+    path("apple-touch-icon.png", RedirectView.as_view(url='/static/logo.png', permanent=True)),
+    path("apple-touch-icon-precomposed.png", RedirectView.as_view(url='/static/logo.png', permanent=True)),
+    re_path(r"^favicons/.*$", RedirectView.as_view(url='/static/logo.png', permanent=True)),
 ]
 
 if settings.DEBUG:
