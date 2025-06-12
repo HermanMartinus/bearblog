e38bd7aceb87487a8342adeece04f106eb9edf84
Herman Martinus <hfbmartinus@gmail.com>
2025-06-12 12:22:41 +0200

    update: middleware holding on to thread for too long

--- conf/settings.py
+++ conf/settings.py
@@ -83,7 +83,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blogs.middleware.LongRequestMiddleware',
+    # 'blogs.middleware.LongRequestMiddleware',
     'blogs.middleware.RequestPerformanceMiddleware',
     'allauth.account.middleware.AccountMiddleware',
 ]
