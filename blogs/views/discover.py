d9e03d8c6b9534ccd17624e4d52dc7824d51ea81
Herman Martinus <hfbmartinus@gmail.com>
2025-07-16 09:15:18 +0200

    update: adding greek to lang filter

--- blogs/views/discover.py
+++ blogs/views/discover.py
@@ -119,7 +119,7 @@ def discover(request):
 
 
 def get_available_languages():
-    return ["cs", "de", "en", "es", "fi", "fr", "hu", "id", "it", "ja", "ko", "nl", "pl", "pt", "ru", "sv", "tr", "zh"]
+    return ["cs", "de", "el", "en", "es", "fi", "fr", "hu", "id", "it", "ja", "ko", "nl", "pl", "pt", "ru", "sv", "tr", "zh"]
 
 
 # RSS/Atom feed
