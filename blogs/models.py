8525fbb5ef8fbf541043be08de401d3b6902e100
Herman Martinus <hfbmartinus@gmail.com>
2025-10-16 12:02:12 +0200

    update: posts in 12 hours instead

--- blogs/models.py
+++ blogs/models.py
@@ -92,7 +92,7 @@ class Blog(models.Model):
     reviewer_note = models.TextField(blank=True)
     hidden = models.BooleanField(default=False, db_index=True)
     flagged = models.BooleanField(default=False, db_index=True)
-    posts_in_last_24_hours = models.IntegerField(default=0, db_index=True)
+    posts_in_last_12_hours = models.IntegerField(default=0, db_index=True)
 
     @property
     def older_than_one_day(self):
@@ -234,8 +234,8 @@ class Blog(models.Model):
             # Update last posted
             self.last_posted = self.posts.filter(publish=True, published_date__lt=timezone.now()).order_by('-published_date').values_list('published_date', flat=True).first()
 
-            # Update posts in last 24 hours
-            self.posts_in_last_24_hours = self.posts.filter(published_date__gte=timezone.now() - timezone.timedelta(hours=24), published_date__lte=timezone.now(), publish=True, make_discoverable=True).count()
+            # Update posts in last 12 hours
+            self.posts_in_last_12_hours = self.posts.filter(published_date__gte=timezone.now() - timezone.timedelta(hours=12), published_date__lte=timezone.now(), publish=True, make_discoverable=True).count()
 
         # Save the blog
         super(Blog, self).save(*args, **kwargs)
