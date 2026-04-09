a686de343b33d4646b652ca1586aad7ed03c2677
Herman Martinus <hfbmartinus@gmail.com>
2026-04-09 08:48:57 +0200

    update: bump django to 6.0.4

--- conf/settings.py
+++ conf/settings.py
@@ -76,6 +76,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django.contrib.postgres',
     'blogs.apps.BlogsConfig',
     'allauth.account',
     'allauth.socialaccount',
