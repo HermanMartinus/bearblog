47b5f0c7c9fb51d275315af2b139af7c32e31c5f
Herman Martinus <hfbmartinus@gmail.com>
2026-04-28 18:12:35 +0900

    update: removed heartbeat code

--- blogs/apps.py
+++ blogs/apps.py
@@ -3,10 +3,3 @@ from django.apps import AppConfig
 
 class BlogsConfig(AppConfig):
     name = 'blogs'
-
-    def ready(self):
-        # TODO: Investigate whether this will work robustly
-        # from blogs.scheduler import start_heartbeat
-        # start_heartbeat()
-        pass
-
