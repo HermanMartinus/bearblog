3a6796cb2a8ad42321fc67081b11103334ac1f87
Herman Martinus <hfbmartinus@gmail.com>
2026-05-09 12:05:29 +0900

    update: prevent multiple blogs server side

--- blogs/views/studio.py
+++ blogs/views/studio.py
@@ -29,7 +29,9 @@ def list(request):
     if request.method == "POST":
         form = BlogForm(request.POST)
         if form.is_valid():
-            if blogs.count() >= request.user.settings.max_blogs:
+            if not request.user.settings.upgraded and blogs.count() >= 1:
+                form.add_error('title', 'Upgrade to create more than one blog.')
+            elif blogs.count() >= request.user.settings.max_blogs:
                 form.add_error('title', 'You have reached the maximum number of blogs.')
             else:
                 subdomain = slugify(form.cleaned_data['subdomain'])
