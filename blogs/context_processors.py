d9c9eb3ae6c7c0033ae60005ac8b29e155cb6be8
Ân Đoàn <33853760+andoan16@users.noreply.github.com>
2026-05-09 09:58:11 +0700

    fix: resolve #349 — Auto-discovery on every page
    Fixes #349
    
    Signed-off-by: Ân Đoàn <33853760+andoan16@users.noreply.github.com>

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,9 +1,11 @@
 import os
 
-# Add tz and admin_passport to every page
+# Add tz, admin_passport, and blog context to every page
 def extra(request):
+    blog = getattr(request, 'blog', None)
     return {
         'tz': request.COOKIES.get('timezone', 'UTC'),
         'admin_passport': request.COOKIES.get('admin_passport') == os.getenv('ADMIN_PASSPORT'),
-        'bear_root': 'http://' + os.getenv('MAIN_SITE_HOSTS').split(',')[0]
+        'bear_root': 'http://' + os.getenv('MAIN_SITE_HOSTS').split(',')[0],
+        'blog': blog
     }
