5c7cd256ae29cac3913e46aabf2b6aeebe82baba
Herman Martinus <hfbmartinus@gmail.com>
2026-08-06 23:10:52 +0200

    update: adding tor list

--- blogs/views/upvotes.py
+++ blogs/views/upvotes.py
@@ -1,3 +1,8 @@
+from time import time
+
+import requests
+from requests.exceptions import RequestException
+
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
@@ -52,6 +57,10 @@ def upvote(request):
         print("Not upvoting: Honeypot filled")
         return response
 
+    if _request_from_tor(request):
+        print("Not upvoting: Tor exit")
+        return response
+
     hash_id = salt_and_hash(request, 'year')
     if not valid_upvote_token(request.POST.get("token", ""), uid, hash_id):
         print("Not upvoting: Invalid token")
@@ -73,3 +82,38 @@ def upvote(request):
         print("Not upvoting: Duplicate upvote")
 
     return response
+
+
+_tor_exit_cache = {'ips': frozenset(), 'expires': 0}
+TOR_EXIT_LIST_URL = 'https://check.torproject.org/torbulkexitlist'
+
+
+def _tor_exit_ips():
+    if time() < _tor_exit_cache['expires']:
+        return _tor_exit_cache['ips']
+    try:
+        response = requests.get(TOR_EXIT_LIST_URL, timeout=3)
+        response.raise_for_status()
+        ips = frozenset(response.text.split())
+        ttl = 60 * 60
+    except RequestException:
+        # Fail open: keep whatever we had, retry soon rather than block everyone
+        ips = _tor_exit_cache['ips']
+        ttl = 60
+    _tor_exit_cache['ips'] = ips
+    _tor_exit_cache['expires'] = time() + ttl
+    return ips
+
+
+def _request_from_tor(request):
+    exit_ips = _tor_exit_ips()
+    if not exit_ips:
+        return False
+
+    candidates = {request.META.get('HTTP_CF_CONNECTING_IP', '').strip()}
+    candidates.update(
+        part.strip() for part in request.META.get('HTTP_X_FORWARDED_FOR', '').split(',')
+    )
+    candidates.discard('')
+
+    return any(ip in exit_ips for ip in candidates)
