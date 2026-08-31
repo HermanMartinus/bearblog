7c188d7b03b86204dbe7be2b70c37364f48ee87d
Herman Martinus <hfbmartinus@gmail.com>
2026-08-31 14:23:16 +0200

    update: drop quicksubmission time to 3 seconds

--- blogs/views/upvotes.py
+++ blogs/views/upvotes.py
@@ -17,7 +17,7 @@ upvote_signer = TimestampSigner(salt='upvote')
 # Tokens are minted per page load, so this only needs to outlast a reading session
 # Form is prepopulates with uid which is invalid, then gets swapped out with a valid one
 UPVOTE_TOKEN_MAX_AGE = 60 * 60 * 12
-UPVOTE_TOKEN_MIN_AGE = 5
+UPVOTE_TOKEN_MIN_AGE = 3
 
 
 def valid_upvote_token_age(token, uid, hash_id):
